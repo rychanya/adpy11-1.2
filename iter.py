e33725d7a6a6bd7ebaa5504db8869e23a9c95f33
rychanya <rychanya@gmail.com>
2020-03-25 18:04:11 +0300

    init

---
+++ iter.py
@@ -0,0 +1,53 @@
+import json
+import requests
+
+
+class CountriesIter:
+
+    def __init__(self, path):
+        self._start = -1
+        self.session = requests.Session()
+        with open(path, mode='r', encoding='utf-8') as file:
+            data = json.load(file)
+            self.countries_names = [iteam['name']['common'] for iteam in data]
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self._start += 1
+        try:
+            country_name = self.countries_names[self._start]
+            print(f'{self._start + 1} of {len(self.countries_names)}')
+            return f'{country_name} - {self.get_url(country_name)}'
+        except IndexError:
+            raise StopIteration
+
+    def get_url(self, country_name):
+        URL = 'https://en.wikipedia.org/w/api.php'
+        PARAMS = {
+            'action': 'query',
+            'format': 'json',
+            'list': 'search',
+            'srlimit': 1,
+            'srsearch': country_name
+        }
+        respose = self.session.get(url=URL, params=PARAMS)
+        page_id = respose.json()['query']['search'][0]['pageid']
+
+        PARAMS = {
+            'action': 'query',
+            'format': 'json',
+            'prop': 'info',
+            'pageids': page_id,
+            'inprop': 'url'
+        }
+        respose = self.session.get(url=URL, params=PARAMS)
+        return respose.json()['query']['pages'][str(page_id)]['canonicalurl']
+
+
+if __name__ == '__main__':
+    countries = CountriesIter('countries.json')
+    with open('out.txt', mode='w', encoding='utf-8') as file:
+       for country in countries:
+           file.write(country + '\n')
